e3f68f0940c9e0bce98db05c9a59c462701f3df6
Junhan <junhanoct@gmail.com>
2020-04-08 23:50:02 -0700

    make it browser action and simplify codebase

--- copy_engine.js
+++ copy_engine.js
@@ -1,14 +1,37 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {urlMatches: 'https://.*google.com'},
-          })
-        ],
-        actions: [ new chrome.declarativeContent.ShowPageAction()]
+function matchURL() {
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url;
+    chrome.storage.sync.get(null, function(options){
+      var used_url_pattern = '';
+      var used_pattern = '';
+
+      var content = '';
+      for(let [url_regex, pattern_regex] of Object.entries(options)) {
+        if (!url.match(url_regex)) {
+          continue;
+        }
+        content = url.match(pattern_regex);
+        used_url_regex = url_regex;
+        used_pattern_regex = pattern_regex;
+        break;
       }
-    ]);
+
+      if (!content) {
+        alert('no match pattern added');
+      } else {
+        var el = document.createElement('textarea');
+        el.value = content;
+        el.setAttribute('readonly', '');
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy')
+        document.body.removeChild(el);
+        alert('copied');
+      }
+    });
   });
+}
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  matchURL();
 });
